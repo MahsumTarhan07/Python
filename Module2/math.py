f2f2249a6b5f0ca8fbe1464c7d87de4689edc636
MahsumTarhan07 <mahsumtarhan1@gmail.com>
2023-04-30 23:36:45 +0300

    math-module-01

---
+++ Module2/math.py
@@ -0,0 +1,28 @@
+#import math  //yontem 1
+#import math as islem  // yontem 2
+
+
+
+#value = dir(math)
+#value = help(math)
+#value = help(math.factorial)
+
+#value = math.sqrt(50)
+#value = math.factorial(5)
+#value = math.fmod(5)
+#value = math.floor(5.9)
+#value = math.ceil(5.9)
+
+
+
+#yontem 3
+
+#from math import * 
+from math import factorial,sqrt
+
+
+value = factorial(5)
+value = sqrt(5)
+
+
+print(value)
