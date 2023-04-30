4606b77231cb570cb2e9e64d22c1bbffb5170386
MahsumTarhan07 <mahsumtarhan1@gmail.com>
2023-05-01 00:03:44 +0300

    My-Module-Demo

---
+++ Module2/mod.py
@@ -0,0 +1,23 @@
+'''
+    Modül hakkında bilgilendirme
+'''
+print("Modül Eklendi")
+number = 20
+
+numbers = [1,2,3]
+
+person = {
+    "name":"ALİ",
+    "age":"34",
+    "city":"Mardin"
+}
+
+def func(x):
+    '''
+        func hakkında bilgilendirme
+    '''
+    print(f'x: {x}')
+    
+class Person:
+    def speak(self):
+        print("Merhaba")
